ac08a8241bcce6524ecb797bb53655ca2212eec8
myyerrol <myyerrol@126.com>
2021-10-12 22:24:59 +0800

    doc: delete unless files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/myyerrol/Workspaces/tree-core-website/node_modules/infima/dist/css/default/default.css"),
-  require("/home/myyerrol/Workspaces/tree-core-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/myyerrol/Workspaces/tree-core-website/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/myyerrol/Workspaces/tree-core-website/src/css/custom.css"),
-];
